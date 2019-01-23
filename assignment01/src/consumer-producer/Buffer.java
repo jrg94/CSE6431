39ac5a1271c9f10d7db380bdeff539899ef6eaf5
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-01-23 11:28:53 -0500

    Implemented produce method

--- assignment01/src/consumer-producer/Buffer.java
+++ assignment01/src/consumer-producer/Buffer.java
@@ -1,8 +1,27 @@
 public class Buffer {
-  private int[] buffer;
+  private ArrayList<Integer> buffer;
+  private boolean ready;
 
+  public Buffer() {
+    this.buffer = new ArrayList<Integer>();
+    this.ready = false;
+  }
+
+  /**
+   * A production method which generates a random value between 0 and 1000
+   * and places it at the end of shared memory.
+   */
   public synchronized void produce() {
-
+    while (ready) {
+      try {
+        wait();
+      } catch (InterruptedException e) {
+        System.out.println("Producer awoken!");
+      }
+    }
+    this.buffer.add((int)(Math.random() * 1000));
+    this.ready = true;
+    notifyAll();
   }
 
   public synchronized int consume() {
