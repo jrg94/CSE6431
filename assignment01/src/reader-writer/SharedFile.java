c65c5e57304aacd6e8a939496d2f6fd4ecf9e7cf
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-01-23 11:58:28 -0500

    Added method stubs

---
+++ assignment01/src/reader-writer/SharedFile.java
@@ -0,0 +1,25 @@
+public class SharedFile {
+
+  private boolean busy;
+
+  public SharedFile() {
+    this.busy = false;
+  }
+
+  public synchronized void startRead() {
+
+  }
+
+  public synchronized void endRead() {
+
+  }
+
+  public synchronized void startWrite() {
+
+  }
+
+  public synchronized void endWrite() {
+    
+  }
+
+}
