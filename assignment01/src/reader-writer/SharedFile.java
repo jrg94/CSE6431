c33e9494ae62c636d257c3cba09dd60334f8aa3c
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-01-23 12:01:08 -0500

    Implemented startRead

--- assignment01/src/reader-writer/SharedFile.java
+++ assignment01/src/reader-writer/SharedFile.java
@@ -1,13 +1,23 @@
 public class SharedFile {
 
   private boolean busy;
+  private int readCount;
 
   public SharedFile() {
     this.busy = false;
+    this.readCount = 0;
   }
 
   public synchronized void startRead() {
-
+    while (busy) {
+      try {
+        wait();
+      } catch (InterruptedException e) {
+        System.out.println("Reader wants to read!");
+      }
+    }
+    readCount++;
+    notifyAll();
   }
 
   public synchronized void endRead() {
@@ -19,7 +29,7 @@ public class SharedFile {
   }
 
   public synchronized void endWrite() {
-    
+
   }
 
 }
