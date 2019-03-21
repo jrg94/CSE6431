8c6045a8486960cc25c119ba542f76ddb54427de
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-03-20 21:49:08 -0400

    Toying with file parsing

--- restaurant/src/main/java/osu/cse6431/Restaurant.java
+++ restaurant/src/main/java/osu/cse6431/Restaurant.java
@@ -3,6 +3,9 @@
  */
 package osu.cse6431;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * @author Jeremy Grifski
  *
@@ -13,8 +16,15 @@ public class Restaurant {
      * @param args
      */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        Scanner input = new Scanner(System.in);
+        int numberOfDiners = Integer.parseInt(input.nextLine());
+        int numberOfTables = Integer.parseInt(input.nextLine());
+        int numberOfCooks = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < numberOfDiners; i++) {
+            String diner = input.nextLine();
+            int[] values = Arrays.stream(diner.split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+        input.close();
     }
 
 }
