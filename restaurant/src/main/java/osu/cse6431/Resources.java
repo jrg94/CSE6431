2715783f41840e9b9683223e1c77a4037ca10bc0
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-03-21 00:04:07 -0400

    Generated resource code

---
+++ restaurant/src/main/java/osu/cse6431/Resources.java
@@ -0,0 +1,73 @@
+package osu.cse6431;
+
+public class Resources {
+
+    private int availableTableCount;
+    private boolean isGrillAvailable;
+    private boolean isFryerAvailable;
+    private boolean isSodaMachineAvailable;
+
+    public Resources(int availableTableCount) {
+        this.setAvailableTableCount(availableTableCount);
+        this.setGrillAvailable(true);
+        this.setFryerAvailable(true);
+        this.setSodaMachineAvailable(true);
+    }
+
+    /**
+     * @return the availableTableCount
+     */
+    public int getAvailableTableCount() {
+        return availableTableCount;
+    }
+
+    /**
+     * @param availableTableCount the availableTableCount to set
+     */
+    private void setAvailableTableCount(int availableTableCount) {
+        this.availableTableCount = availableTableCount;
+    }
+
+    /**
+     * @return the isGrillAvailable
+     */
+    public boolean isGrillAvailable() {
+        return isGrillAvailable;
+    }
+
+    /**
+     * @param isGrillAvailable the isGrillAvailable to set
+     */
+    private void setGrillAvailable(boolean isGrillAvailable) {
+        this.isGrillAvailable = isGrillAvailable;
+    }
+
+    /**
+     * @return the isFryerAvailable
+     */
+    public boolean isFryerAvailable() {
+        return isFryerAvailable;
+    }
+
+    /**
+     * @param isFryerAvailable the isFryerAvailable to set
+     */
+    private void setFryerAvailable(boolean isFryerAvailable) {
+        this.isFryerAvailable = isFryerAvailable;
+    }
+
+    /**
+     * @return the isSodaMachineAvailable
+     */
+    public boolean isSodaMachineAvailable() {
+        return isSodaMachineAvailable;
+    }
+
+    /**
+     * @param isSodaMachineAvailable the isSodaMachineAvailable to set
+     */
+    private void setSodaMachineAvailable(boolean isSodaMachineAvailable) {
+        this.isSodaMachineAvailable = isSodaMachineAvailable;
+    }
+
+}
