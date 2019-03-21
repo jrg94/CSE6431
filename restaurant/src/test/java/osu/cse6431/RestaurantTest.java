52d88fe8a755a36fa4bdb68c0c233960ea6916a3
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-03-21 13:10:14 -0400

    Patched test code

--- restaurant/src/test/java/osu/cse6431/RestaurantTest.java
+++ restaurant/src/test/java/osu/cse6431/RestaurantTest.java
@@ -2,31 +2,24 @@ package osu.cse6431;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
+import java.util.Scanner;
 
 import org.junit.Test;
 
 public class RestaurantTest {
 
-    private void setInputStream(String fileName) {
-        try {
-            ClassLoader classLoader = getClass().getClassLoader();
-            File file = new File(classLoader.getResource(fileName).getFile());
-            InputStream inContent = new ByteArrayInputStream(Files.readAllBytes(file.toPath()));
-            System.setIn(inContent);
-        } catch (IOException e) {
-            System.err.println("Failed to load resource: " + fileName);
-        }
+    private File getFile(String fileName) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(fileName).getFile());
+        return file;
     }
 
     @Test
     public void testLoad() throws IOException {
-        setInputStream("test_001.txt");
-        Restaurant test = Restaurant.load();
+        Scanner input = new Scanner(getFile("test_001.txt"));
+        Restaurant test = Restaurant.load(input);
         assertEquals("Failed to load number of diners", 3, test.getDiners().size());
         assertEquals("Failed to load number of tables", 5, test.getNumberOfTables());
         assertEquals("Failed to load number of cooks", 1, test.getNumberOfCooks());
