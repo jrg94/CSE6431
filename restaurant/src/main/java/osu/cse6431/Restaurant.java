4715329c254c4c2559420f3d20232c09e4053278
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-03-20 21:37:09 -0400

    Created main restaurant file

---
+++ restaurant/src/main/java/osu/cse6431/Restaurant.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package osu.cse6431;
+
+/**
+ * @author Jeremy Grifski
+ *
+ */
+public class Restaurant {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
