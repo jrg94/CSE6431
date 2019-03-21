2142006237072b38bce0ddbec83fd8885f5a8eca
Jeremy Grifski <jeremy.grifski@gmail.com>
2019-03-21 12:18:02 -0400

    Created order completion methods

--- restaurant/src/main/java/osu/cse6431/Cook.java
+++ restaurant/src/main/java/osu/cse6431/Cook.java
@@ -25,21 +25,41 @@ public class Cook extends Thread {
         return activeDiner;
     }
 
-    public synchronized void cookBurger(Diner order) {
-        if (order.getBurgerOrderCount() > 0) {
-            while (!this.getResources().isGrillAvailable()) {
-                /// wait
-            }
+    public synchronized void cookBurger() {
+        while (!this.getResources().isGrillAvailable()) {
+            /// wait
+        }
+    }
+
+    public synchonized void cookFries() {
+        while(!this.getResources().isFryerAvailable()) {
+            // wait
+        }
+    }
+
+    public synchronized void pourSoda() {
+        while (!this.getResources().isSodaMachineAvailable()) {
+            // wait
         }
     }
 
     public void completeOrder(Diner order) {
-        cookBurger(order);
+        for (int i = 0; i < order.getBurgerOrderCount(); i++) {
+            cookBurger();
+        }
+
+        for (int i = 0; i < order.getFryOrderCount(); i++) {
+            cookFries();
+        }
+
+        for (int i = 0; i < order.getDrinkOrderCount(); i++) {
+            pourSoda();
+        }
     }
 
     public void run() {
-      System.out.println("Cook doing stuff");
-      Diner order = getOrder();
-      completeOrder(order);
+        System.out.println("Cook doing stuff");
+        Diner order = getOrder();
+        completeOrder(order);
     }
 }
