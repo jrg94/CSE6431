470b7a91b96fe4f1f5f0a054beda81de0be1a7c5
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-03-23 19:41:01 -0400

    Added cook tests

---
+++ restaurant/src/test/java/osu/cse6431/AllTests.java
@@ -0,0 +1,11 @@
+package osu.cse6431;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CookTest.class, DinerTest.class, ResourcesTest.class, RestaurantTest.class })
+public class AllTests {
+
+}
